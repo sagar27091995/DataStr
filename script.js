da361cf60d43b4a12639b1f9944c4e7569edf299
sagar <sagarsag2709@gmail.com>
2018-07-11 21:49:21 +0530

    Code refactoring

--- script.js
+++ script.js
@@ -1,142 +1,155 @@
+var arr = [2,1,5,4,3]
+
 //bubbleSort
 // ascending
- function bubbleSort(array){
+
+function bubbleSort(arr)
+{
 	var a = performance.now();
-     var swap;
-     do{
-         swap=false;
-         for(var i=0; i<array.length; i++){
-             if(array[i]>array[i+1]){
-                 var temp = array[i];
-                 array[i] = array[i+1];
-                 array[i+1] = temp;
-                 swap = true;
-             }
-         }
-	 }while(swap);
-	 var b = performance.now();
-     console.log('bubble sort took ' + (b - a) + ' ms.');
- }
+    var swapped;
+    do {
+        swapped = false;
+		for (var i=0; i < arr.length-1; i++) 
+		{
+            if (arr[i] > arr[i+1]) {
+                var temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+                swapped = true;
+            }
+        }
+	} while (swapped);
+	var b = performance.now()
+	console.log('bubbleSort took ' + (b - a) + ' ms.');
+	console.log(arr);
+}
 
 //  descending
-// var array = [1,3,2,4,6,5];
+// var arr = [1,3,2,4,6,5];
 // var i;
 // var j;
 // var k;
-// for(i=0;i<array.length;i++)
+// for(i=0;i<arr.length;i++)
 // {
-//     for(j=i+1;j<array.length;j++)
+//     for(j=i+1;j<arr.length;j++)
 //     {
-//       if(array[i]<array[j])
+//       if(arr[i]<arr[j])
 //       {
-//                 k=array[i];
-//                 array[i]=array[j];
-//                 array[j]=k;
+//                 k=arr[i];
+//                 arr[i]=arr[j];
+//                 arr[j]=k;
 //       }
 //     }
 // }
-// console.log(array);
+// console.log(arr);
 
 //selectionSort
-function selectionSort(array){
-	//this loop shifs tht max data to last loop
-	var a = performance.now();
-	for (var i = array.length - 1; i > 0; i--) {
-		var num = 0;
-		//this loop finds the max data  
-		for (var j = 1; j <= i; j++) {
-			if (array[j] > array[num])
-				num = j;
-		}
-		//swap
-		array[i] = b;
-		array[num] = a;
+function selectionSort(arr) {
+var a = performance.now();
+var i,j,key,temp;
+for(i=1;i<arr.length;i++){
+	key=arr[i];
+	j= i-1;
+	while(key<arr[j]){
+		temp = arr[j];
+		arr[j] = arr[j+1];
+		arr[j+1]=temp;
 	}
-	var b = performance.now();
-	console.log('selection sort took ' + (b - a) + ' ms.');
-	}
-
+}
+var b = performance.now()
+console.log('selectionSort took ' + (b - a) + ' ms.');
+console.log(arr);
+}
 
 //Inserstion sort
 
-function InserstionSort(array){
+function InserstionSort(arr){
 var a = performance.now();
-for(var i=1; i<array.length; i++){
+for(var i=1; i<arr.length; i++){
     for(var j=0;j < i; j++){
-        if (array[i] < array[j]){
-            var temp = array.splice(i, 1);
-            array.splice(j,0,temp[0]);
+        if (arr[i] < arr[j]){
+            var temp = arr.splice(i, 1);
+            arr.splice(j,0,temp[0]);
         }
 	}
 
 }
 var b = performance.now()
 console.log('InserstionSort took ' + (b - a) + ' ms.');
-return array;
-}
+console.log(arr);
 
+return arr;
+}
 
 //MergeSort
 
-function MergeSort(left_side,right_side){
-var a = performance.now();
-    var i = 0;
-    var j = 0;
-    var result = [];
-
-    while (i < left_side.length || j < right_side.length){
-        if (i === left_side.length){
-            result.push(right_side[j]);
-            j++;
-        }else if (j === right_side.length || left_side[i] <= right_side[j]){
-            result.push(left_side[i]);
-            i++;   
-        }else{
-            result.push(left_side[j]);
-            j++;
-        }
+function mergeSort (arr) {
+	if (arr.length === 1) {
+	  return arr
 	}
-	var b = performance.now();
-	console.log('MergeSort took ' + (b - a) + ' ms.');
-	return result;
-}
+  
+	var middle = Math.floor(arr.length / 2) 
+	var left = arr.slice(0, middle) 
+	var right = arr.slice(middle) 
+    //  console.log(arr);
+	return merge(mergeSort(left), mergeSort(right))
+  }
+  
+  function merge (left, right) {
+	  
+	let result = []
+	let indexLeft = 0
+	let indexRight = 0
+  
+	while (indexLeft < left.length && indexRight < right.length) {
+	  if (left[indexLeft] < right[indexRight]) {
+		result.push(left[indexLeft])
+		indexLeft++
+	  } else {
+		result.push(right[indexRight])
+		indexRight++
+	  }
+	}
+  
+	return result.concat(left.slice(indexLeft)).concat(right.slice(indexRight));
+  }
+  
+  var list = [2, 5, 1, 3, 7, 2, 3, 8, 6, 3]
+  console.log( "mergeSort" + mergeSort(list) ) // [ 1, 2, 2, 3, 3, 3, 5, 6, 7, 8 ]
 
 //Quick Sort
 
-function quick_Sort(MyArray) {
-	var n1 = new Date().getTime();
-	console.log(n1);
+function qSort(arr){
+	var a = performance.now();
+
+	if (arr.length === 0){
+		return;
+	}
 	var left = [];
 	var right = [];
-	var newArray = [];
-	var pivot = MyArray.pop();
-	var output;
-	if (MyArray.length <= 1) { 
-		output = MyArray;
-	} else {
-
-		for (var i = 0; i < MyArray. length; i++) {
-			if (MyArray[i] <= pivot) {
-				left.push(MyArray[i]);
-			} else {
-				right.push(MyArray[i]);
-			}
+	var pivot = arr[0];
+	
+	for (var i=1; i<arr.length; i++){
+		if(arr[i] < pivot){
+			left.push(arr[i]);
+		}else {
+			right.push(arr[i]);
 		}
-		output = newArray.concat(quick_Sort(left), pivot, quick_Sort(right));
 	}
-	var n2 = new Date().getTime();
-	console.log(n2);
-    var output = n2 - n1;
-	return output;
+	// console.log(left);
+	// console.log(right);
+	// return;
+	var b = performance.now();
+	console.log('qSort took ' + (b - a) + ' ms.');
+	console.log(arr);
+	return qSort(left).concat(pivot,qSort(right));
 }
-
 
 
 //-------------------------Searching Algorithms------------------------
 
 // Sequential Search
 // function seqSearch(arr, result) {
-	
 //     for (var i = 0; i < names.length; ++i) {
 //     if (names[i] == result) {
 //     return true;
@@ -179,8 +192,7 @@ function quick_Sort(MyArray) {
 
 
 
-bubbleSort([1,0,4,4,1,0,6,4,6,3,4,3,12,46,45,234,09,67]);
-selectionSort([4,6,0,1,3,5,56,643,3,1]);
-MergeSort([1,3,4], [5,3,2]);
-InserstionSort([4,6,0,1,3,5,56,643,3,1])
-quick_Sort([4,2,7,1,9,4,2,7,1,9,4,2,7,1,9])
+bubbleSort(arr);
+selectionSort(arr);
+InserstionSort(arr)
+qSort(arr);
